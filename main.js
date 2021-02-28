38f702a4319467bed560a49eca30c7b2a78995ec
clara fred <clara.elise.fred@gmail.com>
2021-02-28 18:18:46 +0100

    Fix bugfixes and refactoring

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
+const cityName = document.querySelector('#cityName')
+const weatherDiv = document.querySelector('#weatherResult')
+const venuesDiv = document.querySelector('#venuesResult')
+
 let venuesArr = []
 
 const searchButton = document.querySelector('#searchButton');
 searchButton.addEventListener('click', async () => {
-    let weatherResult = document.querySelector('#weatherResult')
-    let venuesResult = document.querySelector('#venuesResult')
-    weatherResult.innerHTML = ''
-    venuesResult.innerHTML = ''
+    weatherDiv.innerHTML = ''
+    venuesDiv.innerHTML = ''
     let searchInput = document.querySelector('#searchBar').value
     try {
         const weatherData = await fetchWeatherData(searchInput)
         const venuesData = await fetchVenuesData(searchInput)
-        console.log(weatherData)
-        console.log(venuesData)
-        document.querySelector('#cityName').innerHTML = '--> ' + venuesData.response.geocode.where
-        weatherResult.innerHTML = createWeatherWidget(weatherData)
         venuesArr = venuesData.response.venues
-        venuesResult.innerHTML = createVenuesWidget(venuesArr)
+        cityName.innerHTML = `--> ${venuesData.response.geocode.where}, ${venuesData.response.geocode.feature.cc}`
+        weatherDiv.innerHTML = createWeatherWidget(weatherData)
+        venuesDiv.innerHTML = createVenuesWidget(venuesArr)
     }
     catch (error) {
-        document.querySelector('#cityName').innerHTML = ''
-        weatherResult.innerHTML = `
+        venuesArr = []
+        cityName.innerHTML = ''
+        cityName.innerHTML = `
         <div>
             <h3 id="fail">Unable to find information on "${searchInput}", please try again later.</h3>
         </div>
@@ -28,8 +29,8 @@ searchButton.addEventListener('click', async () => {
 
 const weatherId = 'c80f00a7693e6f405f768dd8ad021d4e'
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?&appid=' + weatherId
-const fetchWeatherData = async (cityName) => {
-    const resp = await fetch(weatherUrl + '&q=' + cityName)
+const fetchWeatherData = async (searchInput) => {
+    const resp = await fetch(weatherUrl + '&q=' + searchInput)
     if (resp.ok) {
         const data = await resp.json()
         return data
@@ -41,8 +42,8 @@ const fetchWeatherData = async (cityName) => {
 const foursquareId = '5WQ2W5CLNTXQ30FPICSFUBKGUH2DMFJB2ETOB0SRQN4MPRVT'
 const foursquareSecret = 'ALTEC3IH5MSCZXOWTDDZKJAIRDY4NPPX5DGZWMNPQXGQ5RXZ'
 const foursquareUrl = `https://api.foursquare.com/v2/venues/search?&client_id=${foursquareId}&client_secret=${foursquareSecret}`
-const fetchVenuesData = async (cityName) => {
-    const resp = await fetch(foursquareUrl + '&near=' + cityName + '&limit=3&v=20210225')
+const fetchVenuesData = async (searchInput) => {
+    const resp = await fetch(foursquareUrl + '&near=' + searchInput + '&limit=3&v=20210225')
     if (resp.ok) {
         const data = await resp.json()
         return data;
@@ -60,7 +61,7 @@ const createWeatherWidget = (data) => {
     return `
     <div class="weatherWidget">
         <h3>Current weather</h3>
-        <img src="${iconUrl}">
+        <img src="${iconUrl}" id="iconWeather">
         <p>Condition: ${condition}</p>
         <p>Temperature: ${temp} degrees C</p>
     </div>
@@ -68,66 +69,61 @@ const createWeatherWidget = (data) => {
 };
 
 const createVenuesWidget = (arr) => {
-    let venuesResult = document.querySelector('#venuesResult')
-    venuesResult.innerHTML = ''
-    let v1 = arr[0]
-    let v2 = arr[1]
-    let v3 = arr[2]
-    return `
-    <div class="venuesWidget">
-        <h3 id=attractions>TOP ATTRACTIONS ></h3>
-        <div id=venues>
-            <h3>${v1.name}</h3>
-            <p>Address:</p>
-            <p>${v1.location.address}</p>
-            <p>${v1.location.city}</p>
-            <img src="${v1.categories[0].icon.prefix}64${v1.categories[0].icon.suffix}" id="icon"></img>
-        </div>
-        <div id=venues>
-            <h3>${v2.name}</h3>
-            <p>Address:</p>
-            <p>${v2.location.address}</p>
-            <p>${v2.location.city}</p>
-            <img src="${v2.categories[0].icon.prefix}64${v2.categories[0].icon.suffix}" id="icon"></img>
-        </div>
-        <div id=venues>
-            <h3>${v3.name}</h3>
-            <p>Address:</p>
-            <p>${v3.location.address}</p>
-            <p>${v3.location.city}</p>
-            <img src="${v3.categories[0].icon.prefix}64${v3.categories[0].icon.suffix}" id="icon"></img>
-        </div>
-    </div>
-    `
+    if (arr.length > 1) {
+        let arrWidget = arr.map((e) => {
+            return `
+            <div id=venues>
+                <h3>${e.name}</h3>
+                <p>Address:</p>
+                <p>${e.location.address}</p>
+                <p>${e.location.city}</p>
+                <img src="${e.categories[0].icon.prefix}64${e.categories[0].icon.suffix}" id="icon"></img>
+            </div>
+            `
+        });
+        return `
+            <div class="venuesWidget">
+                <h3 id=attractions>TOP ATTRACTIONS ></h3>
+                ${arrWidget[0]}
+                ${arrWidget[1]}
+                ${arrWidget[2]}
+            </div>
+            `
+    } else {
+        return venuesDiv.innerHTML = ''
+    }
 };
 
 const weatherCheckbox = document.querySelector('#weatherCheckbox')
 weatherCheckbox.addEventListener('change', function() {
   if (this.checked) {
-    venuesResult.style.display='none'
+    venuesDiv.style.display='none'
   } else {
-    venuesResult.style.display='inline'
+    venuesDiv.style.display='inline'
   }
 });
 
 const venuesCheckbox = document.querySelector('#venuesCheckbox')
 venuesCheckbox.addEventListener('change', function() {
   if (this.checked) {
-    weatherResult.style.display='none'
+    weatherDiv.style.display='none'
   } else {
-    weatherResult.style.display='inline'
+    weatherDiv.style.display='inline'
   }
 });
 
 const alphaCheckbox = document.querySelector('#alphaCheckbox')
 alphaCheckbox.addEventListener('change', function() {
     if (this.checked) {
-        let venuesArrSorted = venuesArr.sort((a, b) => {
-            if(a.name < b.name) { return -1; }
-            if(a.name > b.name) { return 1; }
+        let venuesArrSorted = [...venuesArr]
+        venuesArrSorted.sort((a, b) => a.name.localeCompare(b.name))
+        /*venuesArrSorted.sort((a, b) => {
+            if(a.name.toLowerCase() < b.name.toLowerCase()) { return -1; }
+            if(a.name.toLowerCase() > b.name.toLowerCase()) { return 1; }
             return 0;
-        })
-
-        venuesResult.innerHTML = createVenuesWidget(venuesArrSorted)
+        })*/
+        venuesDiv.innerHTML = createVenuesWidget(venuesArrSorted)
+    } else {
+        venuesDiv.innerHTML = createVenuesWidget(venuesArr)
     }
 });
